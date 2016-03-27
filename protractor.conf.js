a40cc655ec53b0af6c02d0f86f8c16260c71445d
vasergen <vasergen@gmail.com>
2016-03-28 01:35:36 +0200

    add spec reporter to protractor config, remove dot reporter

--- protractor.conf.js
+++ protractor.conf.js
@@ -1,15 +1,31 @@
 "use strict"
 
 exports.config = {
+
     multiCapabilities: [
         {
             name: 'Chrome',
             browserName: 'chrome'
         }
     ],
+
     framework: 'jasmine',
+
     specs: [
         'test/e2e/**.spec.js'
     ],
+
+    //taken from here: https://github.com/bcaudan/jasmine-spec-reporter/blob/master/docs/protractor-configuration.md
+    onPrepare: function() {
+        var SpecReporter = require('jasmine-spec-reporter');
+        // add jasmine spec reporter
+        jasmine.getEnv().addReporter(new SpecReporter({displayStacktrace: 'all'}));
+    },
+
+    //remove protractor dot reporter: https://github.com/bcaudan/jasmine-spec-reporter/blob/master/docs/protractor-configuration.md
+    jasmineNodeOpts: {
+        print: function() {}
+    },
+
     baseUrl: 'http://localhost:3000/'
 }
