849e6a7a1cc8f55ae309fa28180556521606b3a6
vasergen <vasergen@gmail.com>
2016-03-27 18:55:03 +0200

    add spec reporter

--- karma.conf.js
+++ karma.conf.js
@@ -44,7 +44,20 @@ module.exports = function(config) {
     // test results reporter to use
     // possible values: 'dots', 'progress'
     // available reporters: https://npmjs.org/browse/keyword/karma-reporter
-    reporters: ['progress'],
+    //reporters: ['progress'],
+
+    reporters: ['spec'],
+
+    specReporter: {
+      maxLogLines: 5,
+      suppressErrorSummary: true,
+      suppressFailed: false,
+      suppressPassed: false,
+      suppressSkipped: true
+    },
+
+    plugins: ["karma-phantomjs-launcher", "karma-jasmine", "karma-babel-preprocessor", "karma-spec-reporter"],
+
 
 
     // web server port
