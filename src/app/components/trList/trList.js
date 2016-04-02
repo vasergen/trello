831870b6f8734062fd44a25571e2a87adf52fab8
vasergen <vasergen@gmail.com>
2016-04-02 18:01:37 +0200

    add feature: edit list name

--- src/app/components/trList/trList.js
+++ src/app/components/trList/trList.js
@@ -6,12 +6,53 @@ angular.module("trello")
             list: '=',
             listKey: '<'
         },
-        controller: function(FactoryList, FactoryCards, $state) {
+        controller: function(ServiceHelper, FactoryList, FactoryCards, ServiceEvents, $state) {
+            let self = this
             let boardKey = $state.params.boardKey
             let List = new FactoryList(boardKey, this.listKey)
             let Cards = new FactoryCards(boardKey, this.listKey)
 
+
             this.cards = null
+            this.inputId = ServiceHelper.randomString()
+            this.isEdited = false
+
+            ServiceEvents.subscribe('startEdit', (data) => {
+                if(data.id == self.listKey) {
+                    return null
+                }
+
+                self.resetState()
+            })
+
+            this.publishStartEdit = () => {
+                ServiceEvents.publish('startEdit', {
+                    id: this.listKey
+                })
+            }
+
+            this.resetState = () => {
+                this.isEdited = false
+            }
+
+            this.startEdit = () => {
+                this.isEdited = true
+                this.publishStartEdit()
+            }
+
+            this.save = () => {
+                let item = {
+                    name: this.list.name
+                }
+
+                List.update(item)
+                    .then(self.resetState)
+                    .catch(ServiceHelper.logError)
+            }
+
+            this.cancel = () => {
+                this.resetState()
+            }
 
             this.getSortedCards = (snapshot) => {
                 let cards = snapshot.val()
