08cb2d14cad44ca01ca04dd2ab49ed7d06efc01f
vasergen <vasergen@gmail.com>
2016-04-05 15:46:36 +0200

    delete unneccessary modules: firebase, lodashfp

--- src/app/modules/lodashfp.js
+++
@@ -1,9 +0,0 @@
-"use strict"
-
-angular.module("lodash-fp", [])
-    .factory('_', function() {
-        if(!window._)
-            throw new Error("Error! Lodash is not found")
-
-        return window._
-    })
